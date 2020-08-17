20ac07815b132b50665408e2b1138281d72a1cd8
Ahmed Abdulkader <ahmed-public@hotmail.com>
2020-08-17 22:08:44 +0300

    update 1

--- preprocessing/resnetpreprocessor.py
+++
@@ -1,7 +0,0 @@
-from tensorflow.keras.applications.resnet50 import preprocess_input
-
-
-class ResNetPreprocessor:
-    @staticmethod
-    def preprocess(image):
-        return  preprocess_input(image)
